6b91525f2725823002644aaaa2f68f2abf12247f
paolomeneghelli99-coder <paolo.meneghelli99@gmail.com>
2026-08-06 12:34:54 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,1 +1,7 @@
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+FLIGHT_HUNTER_API_URL = os.getenv("FLIGHT_HUNTER_API_URL")
+FLIGHT_HUNTER_API_TOKEN = os.getenv("FLIGHT_HUNTER_API_TOKEN")
