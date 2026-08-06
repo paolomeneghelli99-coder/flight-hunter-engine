2199c4b054c54aa3138155a0f3d6160d921dea7d
paolomeneghelli99-coder <paolo.meneghelli99@gmail.com>
2026-08-06 17:02:28 +0200

    Update __init__.py

--- connectors/__init__.py
+++ connectors/__init__.py
@@ -1,1 +1,9 @@
+"""Registro dei connettori HTTP disponibili."""
 
+from connectors.base import BaseConnector
+
+CONNECTORS = {
+    "ryanair": lambda: BaseConnector(nome="ryanair"),
+}
+
+__all__ = ["BaseConnector", "CONNECTORS"]
