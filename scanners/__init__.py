7d239c21d3b8e01c38ad22bacd68ac25d80477d3
paolomeneghelli99-coder <paolo.meneghelli99@gmail.com>
2026-08-10 16:18:10 +0200

    Update __init__.py

--- scanners/__init__.py
+++ scanners/__init__.py
@@ -2,9 +2,17 @@
 
 from scanners.base import BaseScanner, Offerta
 from scanners.ryanair import RyanairScanner
+from scanners.volotea import VoloteaScanner
+
 
 SCANNERS = {
     "ryanair": RyanairScanner,
+    "volotea": VoloteaScanner,
 }
 
-__all__ = ["BaseScanner", "Offerta", "SCANNERS"]
+
+__all__ = [
+    "BaseScanner",
+    "Offerta",
+    "SCANNERS",
+]
